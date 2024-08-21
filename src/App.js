100061b4de8937cacee9138d07d2ecfc32d77471
Shubham Verma <shubhamv2010@gmail.com>
2024-08-21 16:45:01 +0530

    Initial Setup of App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="flex">
+      <div>
+        <button>Hit Me</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
